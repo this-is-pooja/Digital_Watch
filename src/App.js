46b6affbf5bf579894eb2304bcbd796b8d1ac530
this-is-pooja <poojasoliwal2222@gmail.com>
2021-08-27 20:04:46 +0530

    Digital Watch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Img from "./Image/Digital.jpg";
+import {useState} from "react";
 function App() {
+  let time;
+  const [ctime,setCtime]=useState(time);
+  const UpdateTime=()=>{
+    time=new Date().toLocaleTimeString();
+    setCtime(time);
+  };
+  setInterval(UpdateTime,1000);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={Img} width="100%"height="749.95rem" alt="Digital_Watch" />
+        <div style={{ position: "absolute",top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}>
+          <h1 style={{fontSize:"6rem",color:"white",letterSpacing:"0.2rem"}}>{ctime}</h1>
+        </div>
     </div>
   );
 }
